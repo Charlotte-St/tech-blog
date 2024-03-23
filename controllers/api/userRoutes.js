19b1be3efafa251f06930e85c812552d8d330e16
Lotte-Et <charlotte.stowe@gmail.com>
2024-03-23 17:04:05 -0400

    Still troubleshooting API issues

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,11 +1,16 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+//Register for the site
 router.post('/', async (req, res) => {
   try {
-    const userData = await User.create(req.body);
+    const userData = await User.create({
+      username: req.body.username,
+      password: req.body.password
+    });
 
     req.session.save(() => {
+      req.session.user_id = userData.id;
       req.session.user_id = userData.id;
       req.session.logged_in = true;
 
