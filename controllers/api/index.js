a89f641c1a464875915876a0063b7cbb0558d8fd
Lotte-Et <charlotte.stowe@gmail.com>
2024-03-23 19:13:46 -0400

    Got the login form to work

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,12 +1,12 @@
 const router = require('express').Router();
 
-const userRoutes = require('./userRoutes');
+//const userRoutes = require('./userRoutes');
 const commentRoutes = require('./commentRoutes');
 const postRoutes = require('./postRoutes');
 
 const testRoutes = require('./testRoutes');
 
-router.use('/user', userRoutes);
+//router.use('/user', userRoutes);
 router.use('/comment', commentRoutes);
 router.use('/post', postRoutes);
 router.use('/test', testRoutes);
