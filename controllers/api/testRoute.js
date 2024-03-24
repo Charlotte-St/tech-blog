4e61230547e98498b6deec9eb1fe1f1a94b07858
Lotte-Et <charlotte.stowe@gmail.com>
2024-03-24 11:54:44 -0400

    Added a new page for adding new posts

--- controllers/api/testRoute.js
+++ controllers/api/testRoute.js
@@ -11,7 +11,7 @@ router.get('/', (req, res) => {
 
 //Add a new post
 
-router.post('/', async (req, res)=>{
+router.post('/new', async (req, res)=>{
     try{
         const userData = await Post.create(req.body);
         res.status(200).json(userData);
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -67,6 +67,25 @@ router.get('/dashboard', withAuth, async (req, res) => {
   }
 });
 
+router.get('/new', withAuth, async (req, res) => {
+  try {
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('new', {
+      user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 router.get('/login', (req, res) => {
   if (req.session.logged_in) {
     res.redirect('/');
